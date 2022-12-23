2725ac0bb58ba16eea2d6e93a3b43ea70b4475b8
senaataoglu <ataoglusenaa@gmail.com>
2022-12-23 21:04:43 +0100

    homework_js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const name ='Sena';
+const surname ='Ataoglu';
+
+var fullname = name + ' ' + surname;
+
+    console.log(fullname);
+    document.write(fullname + '</br>');
+
+
+
+for(let i=0; i < 100; i++){
+    if(i%2 === 0){
+        console.log(i);
+        document.writeln(i);
+    }
+    
+}
